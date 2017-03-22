05d00864b006ad1cf2d2bdd688712d5452ce054b
Denis Gorobzeev <denis.gorobzeev@gmail.com>
2017-03-22 13:22:07 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+To start project:
+
+1. npm install
+
+2. npm start
---
+++ src/components/NotFound.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+export default class NotFound extends Component {
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						Страница не найдена.
+						Вернутсья на <Link to="/">главную</Link>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
