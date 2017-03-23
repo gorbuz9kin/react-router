e6fc0d83a7867c8cd5c99439d1e616dd0e4498d9
Denis Gorobzeev <denis.gorobzeev@gmail.com>
2017-03-23 15:07:00 +0200

    comlete course

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,8 @@ import Genre from './components/Genre'
 import Release from './components/Release'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
-import Login from './components/Login'
+import LoginPage from './containers/LoginPage'
+import requireAuthentication from './containers/AuthenticatedComponent'
 
 
 
@@ -17,12 +18,12 @@ export const routes = (
 	<div>
 		<Route path='/' component={App}>
 			<IndexRoute component={Home} />
-			<Route path='admin' component={Admin} onEnter={Admin.onEnter} />
+			<Route path='admin' component={requireAuthentication(Admin)} />
 			<Route path='genre/:genre' component={Genre} >
 				<Route path='genre/:genre/:release' component={Release} />
 			</Route>
 			<Route path='list' component={List} />
-			<Route path='login' component={Login} />
+			<Route path='login' component={LoginPage} />
 		</Route>
 		<Route path="*" component={NotFound} />
 	</div>
---
+++ src/store/configureStore.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+import { rootReducer } from '../reducers'
+import { redirect } from '../middleware/redirect'
+
+export default function configureStore() {
+	const store = compose(
+		applyMiddleware(thunkMiddleware),
+		applyMiddleware(createLogger()),
+		applyMiddleware(redirect)
+	)(createStore)(rootReducer)
+	
+	if (module.hot) {
+		// Enable webpack hot module replacement for reducers
+		
+		module.hot.accept('../reducers', () => {
+			const nextRootReducer = require('../reducers').rootReducer
+			store.replaceReducer(nextRootReducer)
+		});
+	}
+	
+	return store
+	
+}
